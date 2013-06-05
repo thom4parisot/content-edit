478721586dc18268c562cf6528059541f0d83f90
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-06-05 15:23:44 +0100

    Packaging

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		// Import package manifest
+		pkg: grunt.file.readJSON("content-edit.jquery.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			dist: {
+				src: ["src/content.js", "src/plugin.js"],
+				dest: "dist/jquery.content-edit.js"
+			},
+			options: {
+				banner: '"use strict"; (function($){',
+				footer: "})(jQuery,document,window);"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: ["dist/*.js", "!dist/*.min.js"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			my_target: {
+				src: ["dist/jquery.content-edit.js"],
+				dest: "dist/jquery.content-edit.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
+
+	grunt.registerTask("default", ["concat", "jshint", "uglify"]);
+	grunt.registerTask("test", ["jshint"]);
+
+};
