946daf46dac612f1c3348c5be253f27463974a2d
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-06-12 14:49:19 +0100

    Added built files for bower inclusion.

---
+++ dist/jquery.content-edit-history.js
@@ -0,0 +1,159 @@
+(function($){ "use strict";/**
+ * Content Edit Revision History constructor.
+ *
+ * It acts as a showcase on how to extend any behavior of the core experience of the Content Edit plugin.
+ * Anyway, it hooks
+ *
+ * @param editable
+ * @constructor
+ */
+function ContentEditHistoryPlugin(editable){
+  /**
+   * Stores a reference to the editable source we work on.
+   * @type {ContentEditPlugin}
+   */
+  this.editable = editable;
+
+  /**
+   * DOM Element used to display the revisions history.
+   * @type {HTMLElement|null}
+   */
+  this.historyElement = null;
+
+  this.init(editable);
+}
+
+/**
+ * Plugin Namespace.
+ * @type {string}
+ */
+ContentEditHistoryPlugin.namespace = "history";
+
+/**
+ * Initialize the plugin for the first time.
+ * It will mostly resolves dependencies and link them together.
+ *
+ * @param {ContentEditPlugin} editable
+ */
+ContentEditHistoryPlugin.prototype.init = function init(editable){
+  this.historyElement = this.resolveHistoryElement(editable);
+};
+
+/**
+ * Resolve the history element location.
+ * By default, a sibling in the same location, otherwise document lookup.
+ *
+ * @param {ContentEditPlugin} editable
+ * @returns {HTMLElement}
+ */
+ContentEditHistoryPlugin.prototype.resolveHistoryElement = function resolveHistoryElement(editable){
+  var el;
+  var $templateElement = $(editable.templateElement);
+  var selector = editable.selector(ContentEditHistoryPlugin.namespace);
+
+  var lookups = [
+    function(s){ return $templateElement.siblings(s); },
+    function(s){ return $(s); }
+  ];
+
+  while(!el && lookups){
+    el = lookups.shift()(selector).get(0);
+  }
+
+  return el;
+};
+
+/**
+ * Hide the history when the Content Edit elements goes in `idle` state.
+ *
+ * @api
+ */
+ContentEditHistoryPlugin.prototype.idle = function idle(){
+  $(this.historyElement).addClass("hidden");
+
+  //this helps to retrieve what is the source we are working on without knowing it directly
+  $(this.historyElement).data("content-edit-source", null);
+};
+
+/**
+ * Display the history when the Content Edit element goes in `editing` state.
+ *
+ * @api
+ */
+ContentEditHistoryPlugin.prototype.editing = function editing(){
+  //this helps to retrieve what is the source we are working on without knowing it directly
+  $(this.historyElement).data("content-edit-source", this.editable);
+
+  $(this.historyElement).removeClass("hidden");
+};
+
+/**
+ * Revert to a specific revision (copy + editable.saving).
+ *
+ * @api
+ * @param {HTMLElement} revisionElement
+ */
+ContentEditHistoryPlugin.prototype.revertRevision = function revertRevision(revisionElement){
+  this.copyRevision(revisionElement);
+
+  this.editable.setState("saving");
+};
+
+/**
+ * Copy a revision into the editable form.
+ *
+ * @api
+ * @param {HTMLElement} revisionElement
+ */
+ContentEditHistoryPlugin.prototype.copyRevision = function revertRevision(revisionElement){
+  this.editable.setContent( $(revisionElement).find("[data-editable-history-revision]").html().trim() );
+};
+
+/**
+ * Hook on the Content Edit element to determine what to do according to its state.
+ *
+ * @api
+ * @param {MouseEvent} event
+ * @param {ContentEditPlugin} editable
+ */
+ContentEditHistoryPlugin.UIStateHandler = function UIStateHandler(event, editable){
+  var history = editable.plugins[ContentEditHistoryPlugin.namespace];
+
+  if (!history){
+    history = editable.plugins[ContentEditHistoryPlugin.namespace] = new ContentEditHistoryPlugin(editable);
+  }
+
+  // where all the magic happens!
+  history.historyElement && history[editable.state] && history[editable.state](event);
+};
+
+/**
+ * Handle *revision* actions and deals with the remote Content Edit form.
+ *
+ * @api
+ */
+ContentEditHistoryPlugin.processRevisionAction = function processRevisionAction(){
+  var ns = ContentEditHistoryPlugin.namespace;
+
+  var editable = $(this).parents("[data-editable-"+ns+"]").data("content-edit-source");
+  var revisionAction = this.getAttribute("data-editable-"+ns+"-action") + "Revision";
+
+  var history = editable.plugins[ns];
+  history.historyElement && history[revisionAction] && history[revisionAction]( $(this).parents(".editable-history-item").get(0) );
+};
+
+/**
+ * Hook Event Delegation on a context.
+ * Usually the context is `$(document)`.
+ *
+ * @static
+ * @param {jQuery} $context
+ */
+ContentEditHistoryPlugin.dispatch = function dispatch($context){
+  $context.on("editable.any", "[data-editable]", ContentEditHistoryPlugin.UIStateHandler);
+  $context.on("click", "a[data-editable-history-action]", ContentEditHistoryPlugin.processRevisionAction);
+};
+
+$.fn.editable.historyPluginConstructor = ContentEditHistoryPlugin;
+
+(ContentEditHistoryPlugin.dispatch)($(document));})(jQuery,document,window);
---
+++ dist/jquery.content-edit.js
@@ -0,0 +1,356 @@
+(function($){ "use strict";/**
+ * Content Edit Plugin Constructor
+ *
+ * Link and initialize elements together.
+ *
+ * @param {HTMLElement} element
+ * @param {Object=} options
+ * @constructor
+ */
+function ContentEditPlugin(element, options) {
+  this.options = $.extend({}, ContentEditPlugin.defaults, options);
+
+  /**
+   * Active state of the content edit.
+   * It is used for a naive state machine approach and transition
+   * @type {string}
+   */
+  this.state = "idle";
+
+  /**
+   * Previous state of the content edit.
+   * Mainly used to detect if we're asked to transition in a new state.
+   * @type {string}
+   */
+  this.previousState = this.state;
+
+  /**
+   * DOM Element from which the edit has been requested (not necessarily the one we edit).
+   * @type {HTMLElement|null}
+   */
+  this.sourceElement = null;
+
+  /**
+   * DOM Element used to let the user edit the content.
+   * @type {HTMLElement|null}
+   */
+  this.templateElement = null;
+
+  /**
+   * Plugins instance holder.
+   * @type {Object}
+   */
+  this.plugins = {};
+
+  /**
+   * Actual value of the editable content.
+   * @type {String}
+   */
+  this.value = "";
+
+  /**
+   * Previous value of the editable content.
+   * Normally filed when saving occurs.
+   * @type {String}
+   */
+  this.oldValue = "";
+
+  //init process. It populates the previous attributes.
+  this.init(element);
+  this.initEvents();
+}
+
+/**
+ * Initialize elements and resolve their dependencies between each other.
+ *
+ * @api
+ */
+ContentEditPlugin.prototype.init = function init (sourceElement) {
+  if (!sourceElement.hasAttribute("data-editable")){
+    sourceElement.setAttribute("data-editable", "");
+    this.options.identifier && sourceElement.setAttribute("data-editable-template", this.options.identifier);
+  }
+
+  this.sourceElement = sourceElement;
+  this.templateElementLookup(this.sourceElement);
+};
+
+/**
+ *
+ * @api
+ * @param {String} namespace
+ * @returns {string}
+ */
+ContentEditPlugin.prototype.selector = function selector (namespace) {
+  return "[data-editable-%ns%='%id%']"
+    .replace("%ns%", namespace)
+    .replace("%id%", this.templateElement.getAttribute("data-editable-template") || "");
+};
+
+/**
+ * Initialize events by… hooking events on the contentElement.
+ * We act this way to let the build process totally modular and expandable at no cost.
+ *
+ * @api
+ */
+ContentEditPlugin.prototype.initEvents = function initEvents () {
+  var $contentElement = $(this.sourceElement);
+
+  $contentElement.on("editable.editing", $.proxy(this.startEdit, this));
+  $contentElement.on("editable.idle", $.proxy(this.endEdit, this));
+  $contentElement.on("editable.saving", $.proxy(this.onSave, this));
+};
+
+/**
+ * Displays the form element and let the user edit the content.
+ * Usually used when transitioning to 'editing' state.
+ *
+ * @api
+ */
+ContentEditPlugin.prototype.startEdit = function startEdit () {
+  $(this.templateElement).data("editable-source", this);
+
+  this.setContent(this.sourceElement.innerHTML);
+
+  $(this.templateElement)
+    .find(".original-content")
+      .text(this.sourceElement.innerHTML)
+      .end()
+    .removeClass(this.options.visibilityTogglingClass);
+};
+
+/**
+ * Hides the form and resets everything back.
+ * Usually used when transitioning back to 'idle' state.
+ *
+ * @api
+ */
+ContentEditPlugin.prototype.endEdit = function endEdit () {
+  $(this.templateElement).data("editable-source", null);
+
+  $(this.templateElement)
+    .addClass(this.options.visibilityTogglingClass)
+    .find(".original-content")
+    .text("");
+
+  this.setContent("");
+};
+
+/**
+ * Hides the form and resets everything back.
+ * Usually used when transitioning back to 'idle' state.
+ *
+ * @api
+ */
+ContentEditPlugin.prototype.onSave = function onSave () {
+  var userValue = $(this.templateElement).find("[data-editable-content]").val();
+
+  if (userValue !== this.value){
+    this.oldValue = this.value;
+    this.value = userValue;
+  }
+};
+
+/**
+ * Set the new content value of the Content Edit form.
+ * It is not saved yet so the user can still alter it before hitting the *save* button.
+ *
+ * @api
+ * @param {String} value
+ */
+ContentEditPlugin.prototype.setContent = function setContent(value){
+  $(this.templateElement).find("[data-editable-content]").val(value);
+
+  this.oldValue = this.value;
+  this.value = value;
+};
+
+/**
+ * Resolve the `templateElement`.
+ * The edit process will happen in the
+ *
+ * @param {HTMLElement} contentElement
+ * @returns {HTMLElement}
+ */
+ContentEditPlugin.prototype.templateElementLookup = function templateElementLookup (contentElement) {
+  var templateSelector = contentElement.getAttribute("data-editable-template") || "";
+
+  this.templateElement = $("form[data-editable-template='"+templateSelector+"']").get(0);
+
+  return this.templateElement;
+};
+
+/**
+ * Transition the edit process in a new state.
+ *
+ * @param {String} newState
+ */
+ContentEditPlugin.prototype.setState = function setState (newState) {
+  if (newState === this.state || !~ContentEditPlugin.states.indexOf(newState)){
+    return false;
+  }
+
+  var $contentElement = $(this.sourceElement);
+
+  this.previousState = this.state;
+  this.state = newState;
+
+  $contentElement.trigger("editable.any", [this]).trigger("editable." + this.state, [this]);
+
+  $contentElement.add(this.templateElement)
+    .removeClass("editable-"+this.previousState)
+    .addClass("editable-"+this.state);
+};
+
+/**
+ * Default plugin options.
+ * Override them to alter *all the future new instances* of content edition.
+ *
+ * @type {Object}
+ */
+ContentEditPlugin.defaults = {
+  identifier: "",
+  visibilityTogglingClass: "hidden",
+  preventDefault: {
+    "a": true,
+    "form": false
+  }
+};
+
+/**
+ * Supported states of the content edition.
+ * If you may add items in there, removing one of these may harm a little penguin.
+ *
+ * @type {Array}
+ */
+ContentEditPlugin.states = ["idle", "editing", "saving"];
+
+/**
+ * Find the editable template contextual to an HTML element.
+ *
+ * @static
+ * @param {HTMLElement} context
+ * @returns {jQuery} The DOM element supposed to contain a link the editable content.
+ */
+ContentEditPlugin.findContextSource = function findContextSource(context){
+  var $this = $(context), el;
+
+  if (!context){
+    return $this;
+  }
+
+  /* jshint validthis:true */
+  if (context.hasAttribute("data-editable-template") && !context.hasAttribute("data-editable")){
+    el = context;
+  }
+
+  if (!el && $this.parents("form[data-editable-template]").length){
+    el = $this.parents("form[data-editable-template]").get(0);
+  }
+
+  if (!el && $this.find("form[data-editable-template]").length){
+    el = $this.find("form[data-editable-template]").get(0);
+  }
+
+  return $(el);
+};
+
+/**
+ * Handles any form submission.
+ *
+ * @todo refactor with cancelEdit
+ * @static
+ * @param {MouseEvent} event
+ */
+ContentEditPlugin.UISubmitHandler = function UISubmitHandler (event) {
+  /* jshint validthis:true */
+  var editable = ContentEditPlugin.findContextSource(this).data("editable-source");
+
+  if (editable.options.preventDefault.form){
+    event.preventDefault();
+  }
+
+  editable.setState("saving");
+};
+
+
+/**
+ * Handles any form cancellation.
+ *
+ * @todo refactor with submitEdit
+ * @static
+ * @param {MouseEvent} event
+ */
+ContentEditPlugin.UICancelHandler = function UICancelHandler (event) {
+  /* jshint validthis:true */
+  var editable = ContentEditPlugin.findContextSource(this).data("editable-source");
+
+  if (editable.options.preventDefault.a){
+    event.preventDefault();
+  }
+
+  editable.setState("idle");
+};
+
+/**
+ * Handle the click on an editable element.
+ * Delegates to a remote editable if it's an hyperlink whose anchor resolves an editable element.
+ *
+ * @static
+ * @param {MouseEvent} event
+ * @constructor
+ */
+ContentEditPlugin.UIEDitHandler = function UIEDitHandler (event) {
+  /* jshint validthis:true */
+
+  var target;
+  var targetSelector = (this.getAttribute("href") || "").trim();
+
+  event.preventDefault();
+  target = (targetSelector[0] === "#" && $(targetSelector).get(0) ) || this;
+
+  $(target).editable();
+};
+
+/**
+ * Hook Event Delegation on a context.
+ * Usually the context is `$(document)`.
+ *
+ * @static
+ * @param {jQuery} $context
+ */
+ContentEditPlugin.dispatch = function dispatch($context){
+  $context.on("click", "[data-editable]", ContentEditPlugin.UIEDitHandler);
+  $context.on("submit", "[data-editable-template]", ContentEditPlugin.UISubmitHandler);
+  $context.on("click", "[data-editable-template] [type='submit']", ContentEditPlugin.UISubmitHandler);
+  $context.on("click", "[data-editable-template] [data-toggle='cancel']", ContentEditPlugin.UICancelHandler);
+};
+
+
+/**
+ * Public jQuery Plugin Declaration.
+ *
+ * @param {Object|String=} options
+ * @returns {jQuery}
+ */
+$.fn.editable = function (options) {
+  return this.each(function () {
+    if (!$.data(this, "plugin_editable")) {
+      $.data(this, "plugin_editable", new ContentEditPlugin(this, typeof options === "object" && options));
+    }
+
+    if (typeof options === "string" || options === undefined) {
+      $.data(this, "plugin_editable").setState(options || "editing");
+    }
+  });
+};
+
+$.fn.editable.Constructor = ContentEditPlugin;
+
+/*
+  Hooking event delegation.
+ */
+(ContentEditPlugin.dispatch)($(document));
+
+
+})(jQuery,document,window);
